2709211c96a0638ec41377e1fece3bdec47b91e8
Kathoni <thrusherkeys@gmail.com>
2024-06-16 01:00:58 +0300

    Adjasments

---
+++ src/compnents/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DashboardOne= "/dashboard";
+
+
+function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-transparent ">
+            
+            <div className="container">
+                <Link className="navbar-brand text-dark " to="/"><strong>Admin</strong></Link>
+                <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link text-dark" to={DashboardOne}> <strong>Dashboard One</strong></Link>
+                        </li>
+                     </ul>   
+                            <form className="d-flex display-flex-right" role="search">
+                              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-success " type="submit">Search</button>
+                      </form>
+                    
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/compnents/Sidebar.jsx
+++ src/compnents/Sidebar.jsx
@@ -1,67 +1,67 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Link } from 'react-router-dom';
+import '../styles/style.css';
+import React, { useState } from 'react';
+
+
 
 function Sidebar() {
-  return (
-    <div >
-        <div className='bg-beige sidebar p-2'>
-    <div className='m-2'>
-    <i class="bi bi-box2-heart me-2 fs-4"></i>
-        <span className='brand-name fs-4 '>Admin</span>
-    </div>
-    <hr className='text-dark'/>
-    <div className='list-group list-group-flush'>
-      <ul>
-        <li>
-    <Link className='list-group-item py-2'>
-        <i className='bi bi-speedometer2 fs-5 me-3'></i>
-        <span className='fs-5'>Dashboard</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item py-2'>
-        <i className='bi bi-house-heart-fill fs-5 me-3'></i>
-        <span className='fs-5'>Home</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item  py-2'>
-        <i className='bi bi-table fs-5 me-3'></i>
-        <span className='fs-5'>Products</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item  py-2'>
-        <i className='bi bi-clipboard-data fs-5 me-3'></i>
-        <span className='fs-5'>Report</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item  py-2'>
-        <i className='bi bi-people fs-5 me-3'></i>
-        <span>Customers</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item  py-2'>
-        <i className='bi bi-power fs-5 me-3'></i>
-        <span className='fs-5'>Log out</span>
-      </Link>
-      </li>
-      <li>
-      <Link className='list-group-item  py-2'>
-        <i className='bi bi-gear-fill fs-5 me-3'></i>
-        <span className='fs-5'>Settings</span>
-      </Link>
-      </li>
-      </ul>
-    </div>
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+    <div className="offcanvas offcanvas-start " tabIndex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="sidebarMenuLabel">Admin</h5>
+        <button type="button" className="btn-close text-reset" onClick={toggleSidebar}></button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <Link className="nav-link"  >
+              <i className="bi bi-speedometer2 me-2"></i>Dashboard
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" >
+              <i className="bi bi-house-heart-fill me-2"></i>Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link"  >
+              <i className="bi bi-table me-2"></i>Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" >
+              <i className="bi bi-clipboard-data me-2"></i>Report
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" >
+              <i className="bi bi-people me-2"></i>Customers
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link"  >
+              <i className="bi bi-power me-2"></i>Log out
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link">
+              <i className="bi bi-gear-fill me-2"></i>Settings
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
 </div>
-</div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
